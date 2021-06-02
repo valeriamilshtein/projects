f7e0119408d41ba41301362cbb53864379ea8d4f
Valeria Milshtein <valeriamilshtein@gmail.com>
2021-06-02 18:59:59 -0400

    Change from 'recipient' to 'recipients'.

--- project_send_email/send_email.py
+++ project_send_email/send_email.py
@@ -4,11 +4,11 @@ from datetime import datetime  # Supplies classes for manipulating dates and tim
 gmail_account = {
     'user': 'valeriiapidpala@gmail.com',
     'password': 'mypassword',
-    'recipient': ['valeriamilshtein@gmail.com', 'kartoshka548@gmail.com']
+    'recipients': ['valeriamilshtein@gmail.com', 'kartoshka548@gmail.com']
 }
 
 sent_from = gmail_account['user']
-to = gmail_account['recipient']
+recipients = gmail_account['recipients']
 subject = 'Super Important Message'
 body = '''
 Hey, I did it!!!
@@ -21,13 +21,13 @@ To: %s
 Subject: %s
 
 %s
-''' % (sent_from, ', '.join(to), subject, body)
+''' % (sent_from, ', '.join(recipients), subject, body)
 
 try:
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     server.ehlo()
     server.login(gmail_account['user'], gmail_account['password'])
-    server.sendmail(sent_from, to, email_text)
+    server.sendmail(sent_from, recipients, email_text)
     server.close()
 
     print('Email sent!')  # If success
