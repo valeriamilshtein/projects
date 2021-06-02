e223077ccacf8570c0649a1fb6536d66ac50ddf7
Valeria Milshtein <valeriamilshtein@gmail.com>
2021-06-02 18:32:43 -0400

    init: email project

---
+++ project_send_email/send_email.py
@@ -0,0 +1,35 @@
+import smtplib  # Handles different parts of the protocol: connecting, authenticating, validation, sending emails
+from datetime import datetime  # Supplies classes for manipulating dates and times
+
+gmail_account = {
+    'user': 'valeriiapidpala@gmail.com',
+    'password': 'mypassword',
+    'recipient': ['valeriamilshtein@gmail.com', 'kartoshka548@gmail.com']
+}
+
+sent_from = gmail_account['user']
+to = gmail_account['recipient']
+subject = 'Super Important Message'
+body = '''
+Hey, I did it!!!
+You asked me about current date... Here we are: 
+''' + datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
+
+email_text = '''\
+From: %s
+To: %s
+Subject: %s
+
+%s
+''' % (sent_from, ', '.join(to), subject, body)
+
+try:
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(gmail_account['user'], gmail_account['password'])
+    server.sendmail(sent_from, to, email_text)
+    server.close()
+
+    print('Email sent!')  # If success
+except:
+    print('Something went wrong!')  # If error
